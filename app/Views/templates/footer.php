e91b63da39c7a0c5eacc5847b51952485ab5615e
Riste Micev <riste.micev1@gmail.com>
2022-04-14 02:35:44 +0200

    basic navigation

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,6 +1,6 @@
 <!-- FOOTER: DEBUG INFO + COPYRIGHTS -->
 
-<footer>
+<footer id="footer">
     <div class="environment">
 
         <p>Page rendered in {elapsed_time} seconds</p>
@@ -19,7 +19,7 @@
 </footer>
 
 <!-- SCRIPTS -->
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <script>
     function toggleMenu() {
         var menuItems = document.getElementsByClassName('menu-item');
@@ -31,8 +31,5 @@
 </script>
 
 <!-- -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </body>
 </html>

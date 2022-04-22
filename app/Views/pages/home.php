e374f86e753e6ce9aad8c6d887cffaa951fc465d
Riste Micev <riste.micev1@gmail.com>
2022-04-22 13:06:57 +0200

    home page

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -1,5 +1,15 @@
-<div class="frame-bg">
-    <div class="image-bg">
-
+<div class="home-bg">
+    <div class="bg-text">
+        Find the barbershop that best suits your style
+    </div>
+    <div class="container">
+        <div class="row justify-content-between home-buttons">
+            <div class="col-4 home-button">
+                Register your barbershop
+            </div>
+            <div class="col-4 home-button">
+                Explore our barbershops
+            </div>
+        </div>
     </div>
 </div>

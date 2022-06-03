c3e03d5c3a915b7366275cdbffac0bdff8cb5843
Riste Micev <riste.micev1@gmail.com>
2022-06-03 19:04:57 +0200

    front-end updates

--- app/Views/user_auth/login.php
+++ app/Views/user_auth/login.php
@@ -1,32 +1,36 @@
 <div class="bg-register">
     <div class="container grayed">
         <div class="container register-sign">
-            Login
+            Sign in
         </div>
         <div class="alert alert-danger" id="greski" hidden>
         </div>
-        <form action="" method="post">
-            <div class="form-group mb-3">
-                <input type="email" name="email" placeholder="Email" value="<?= set_value('email') ?>"
-                       class="form-control">
+        <form action="" method="post" class="mb-5 mx-5 font_poppins">
+            <div class="register__input_form">
+                <div class="form-group mb-3">
+                    <label for="email" class="font_poppins">Email</label>
+                    <input type="email" name="email" placeholder="Email" value="<?= set_value('email') ?>" class="form-control font_poppins">
+                </div>
+                <div class="form-group mb-5">
+                    <label for="password" class="font_poppins">Password</label>
+                    <input type="password" name="password" placeholder="*************" class="form-control font_poppins">
+                </div>
             </div>
-            <div class="form-group mb-3">
-                <input type="password" name="password" placeholder="Password" class="form-control">
-            </div>
-            <button type="submit" class="dugme" id="prati">SIGN IN
+            <button type="submit" class="form__submit_button px-4 py-2" id="prati">
+                SIGN IN
+                <i class="fa fa-arrow-right-long"></i>
             </button>
         </form>
         <div class="d-grid">
-            <a href="register">Don't have an account? Sign up here</a>
+            <a href="register" class="font_poppins">Don't have an account? Sign up here</a>
         </div>
     </div>
 </div>
 
 <script>
+    $(function() {
 
-    $(function () {
-
-        $('form').on('submit', function (e) {
+        $('form').on('submit', function(e) {
             e.preventDefault();
 
             $.ajax({
@@ -34,8 +38,8 @@
                 url: '/user_auth/login',
                 dataType: "html",
                 data: $('form').serialize(),
-                success: function (response) {
-                    if (response==="ok") {
+                success: function(response) {
+                    if (response === "ok") {
                         alert("Login Successful!");
                         window.location = "/afterLogin"
                     } else {
@@ -43,7 +47,7 @@
                         $('#greski').html(response);
                     }
                 },
-                error: function (result) {
+                error: function(result) {
                     $('body').html("err");
                 },
             });
